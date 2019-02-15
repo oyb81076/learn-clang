a1535513e3defe16934b2e9e26155c1b736f0ebe
欧阳斌 <ouyangbin81076@gmail.com>
2019-02-15 17:26:57 +0800

    fy

--- simple/start.sh
+++ simple/start.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-# 监听single目录下的文件, 有文件改变的时候就编译并窒息
-src=`echo $PWD/ | awk '{print length($0)}'`
-mkdir -p build;
-fswatch -x -t -u . \
-  | awk '{if( $0 ~ " IsFile" && $0 ~ " Updated" && $6 ~ ".c$") print $6; system("")}' \
-  | awk '{print substr($0, '$src' + 1); system("")}' \
-  | awk '{print "echo ["$0"] && gcc "$0" -o build/single.o && ./build/single.o"; system("")}' \
-  | bash;
+PWD=`pwd`
+SRC=`echo $PWD/ | awk '{print length($0)}'`
+OUT=$PWD/build/a.out
+mkdir -p build
+#echo -e "\033[32m 绿色字 \033[0m"
+fswatch -x -t -u .\
+    | awk '{if($6 ~ "\.c$") print $0; system("")}'\
+    | awk '{if($0 ~ "Updated") print $0; system("")}'\
+    | awk '{if($0 ~ "IsFile") print $0; system("")}'\
+    | awk '{print $4" "substr($6, '$SRC' + 1);system("")}'\
+    | awk '{print "echo -e \"[\\033[32m"$1"\\033[0m] "$2"\" && gcc "$2" -o '$OUT' && '$OUT'";system("")}'\
+    | bash 
+
